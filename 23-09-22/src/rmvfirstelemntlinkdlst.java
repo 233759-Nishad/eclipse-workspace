5a63a8aa3e1904d05f7ade3d60b2afc7c22b375f
233759-Nishad <113493480+233759-Nishad@users.noreply.github.com>
2022-09-23 16:34:48 +0530

    change

---
+++ 23-09-22/src/rmvfirstelemntlinkdlst.java
@@ -0,0 +1,31 @@
+	import java.util.LinkedList;
+public class rmvfirstelemntlinkdlst {
+  public static void main (String[] args) {
+	        
+	        // Creating an LinkedList
+	        LinkedList<String> list = new LinkedList<String>();
+	  
+	        // Use add() method to add elements in the list
+	        list.add("Geek");
+	        list.add("for");
+	        list.add("Geeks");
+	        list.add("2020");
+	        list.add("2021");
+	  
+	        // Displaying the list
+	        System.out.println("LinkedList:\t" + list);
+	  
+	        // Remove the tail using removeLast()
+	        System.out.println("The last element is removed:\t" + list.removeLast());
+	          
+	        // Displaying the final list
+	        System.out.println("Final LinkedList:\t" + list);
+	          
+	        // Remove the head using remove()
+	        System.out.println("The first element is removed:\t" + list.removeFirst());
+	  
+	        // Displaying the final list
+	        System.out.println("Final LinkedList:\t" + list);
+	    }
+	}
+
