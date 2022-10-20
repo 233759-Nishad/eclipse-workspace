941a8f70b9e91ba11ce0a9a608a6303f422b9f1b
233759-Nishad <113493480+233759-Nishad@users.noreply.github.com>
2022-10-20 18:03:02 +0530

    change

---
+++ 19-10-22/src/New/Pattern2.java
@@ -0,0 +1,16 @@
+package New;
+
+public class Pattern2{
+    public static void main(String args[])
+    {
+        int i,j;
+        for(i=6;i>0;i--)
+        {
+            for(j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
